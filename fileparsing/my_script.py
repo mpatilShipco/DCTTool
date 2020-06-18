bc14c51fcaee9251f425521a87969303edc88368
mpatilShipco <mpatil@shipco.com>
2020-06-18 13:37:27 +0530

    file parsing with python

--- fileparsing/my_script.py
+++ fileparsing/my_script.py
@@ -5,6 +5,14 @@ from openpyxl import load_workbook
 import openpyxl
 import os
 import xlrd as xl
+import xlwt 
+from xlwt import Workbook 
+from xlutils.copy import copy
+from openpyxl import Workbook
+from openpyxl.styles import Color, Fill
+from openpyxl.cell import Cell
+from openpyxl.styles import Font
+from openpyxl.styles.colors import Color
 
 basepath = ' \n ====== Base Path ======= ' + os.path.dirname(os.path.realpath(__file__))  + ' ----------- \n ' ;
 print(basepath);
@@ -93,21 +101,63 @@ add_column('ofr', ['Error'])
 wb.save(full_file_path)
 
 
-#Read Whole excel file using xlrd
+#Read Whole excel file using xlrd - Start
+# loc = (full_file_path)
+# wb = xl.open_workbook(loc)
+# sh = wb.sheet_by_index(0)
 
-loc = (full_file_path)
-wb = xl.open_workbook(loc)
-sh = wb.sheet_by_index(0)
-for i in range(sh.nrows):
- for j in range(sh.ncols):
- 	 #if(j != 'FLEXPORT'):
- 	 	#sh.cell_value[i][mxcl] = 'Invalid Customer'
-  	
-  	# if(i > 1)	:
-  		# break;
-    print (' \n \r cell_value = ',sh.cell_value(i,j))
-    # print (' \n \r cell_value I = ',i)
-    # print (' \n \r cell_value J = ',j)
-    # print (' \n \r mxcl = ',mxcl)
-    
-#wb.save(full_file_path)
+# newwb = copy(wb)
+# sheet1 = newwb.get_sheet(0)
+# mxcl = sh.ncols;
+
+# for i in range(sh.nrows):
+#  for j in range(sh.ncols):
+#  	 if sh.cell_value(i,j) != 'FLEXPORT' and j == 1:
+#  	 	print('If============',sh.cell_value(i,j))
+#  	 	sheet1.write(i, mxcl, 'Invalid Customer')
+ 	 	
+# newwb.save(full_file_path)
+#Read Whole excel file using xlrd - End
+
+
+
+#Read and Write using the openpyxl - Start
+wb=load_workbook(full_file_path)
+sheet=wb.active
+max_row=sheet.max_row
+max_column=sheet.max_column
+print('NEW MAX ROW ',max_row);
+print('NEW MAX COLUMN ',max_column);
+
+# def InvalidCustomer(rownum, columnnum):
+# 	Color.RED = 'FFFF0000';
+# 	_cell = sheet.cell(row=rownum, column=columnnum)
+# 	_cell.style.font.color.index = Color.RED
+# 	_cell.style.font.name = 'Arial'
+# 	_cell.style.font.size = 12
+# 	_cell.style.font.bold = True
+# 	_cell.style.alignment.wrap_text = True
+# 	_cell.style.fill.fill_type = Fill.FILL_SOLID
+# 	_cell.style.fill.start_color.index = Color.DARKRED
+
+for i in range(2,max_row+1):
+    for j in range(1,max_column+1):
+        cell_obj=sheet.cell(row=i,column=j)
+        if cell_obj.value != 'FLEXPORT' and j == 2:
+        	print('Val = ',cell_obj.value)
+	        print('I = ',i)
+	        print('J = ',j)
+	        sheet.cell(row=i, column=max_column).value = 'Invalid Customer'
+	        sheet.cell(row=i, column=max_column).font = Font(color = "FF0000");
+	        # sheet.cell(row=i, column=max_column).font.color = 'FFFF0000'
+	        # sheet.cell(row=i, column=max_column).font.name = 'Verdana'
+	        # sheet.cell(row=i, column=max_column).font.size = 15
+	        # sheet.cell(row=i, column=max_column).font.bold = True
+	        
+	        #InvalidCustomer(i, j)
+wb.save(full_file_path)
+
+        #print(cell_obj.value,end=' | ')
+
+#Read and Write using the openpyxl - End
+
