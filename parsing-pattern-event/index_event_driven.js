09a281836b9d56e8e231a149dc524826abf2d25a
mpatilShipco <mpatil@shipco.com>
2020-06-20 15:53:57 +0530

    added changes

--- parsing-pattern-event/index_event_driven.js
+++ parsing-pattern-event/index_event_driven.js
@@ -5,6 +5,12 @@ let url = "mongodb://localhost:27017/";
 let {PythonShell} = require('python-shell')
 var events = require("events");
 var eventsEmitter = new events.EventEmitter();
+var chokidar = require('chokidar');
+const fsex = require('fs-extra')
+
+// chokidar.watch('.').on('all', (event, path) => {
+//   console.log(event, path);
+// });
 
 const app = express();
  
@@ -13,6 +19,11 @@ global.__basedir = __dirname;
 var cors = require('cors')
 app.use(cors())
 app.options('*', cors())
+
+
+var pathtowatch = __basedir + '/uploads/';
+var watcher = chokidar.watch(pathtowatch, {ignored: /^\./, persistent: true});
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -46,7 +57,7 @@ function readFileContent(fileName){
 
 function  displayFileContent(data){
     console.log("File Data:");
-    console.log(data);
+    //console.log(data);
     eventsEmitter.emit('finished');
 }
 
@@ -59,13 +70,29 @@ function readFile(err,data,fileName) {
     eventsEmitter.emit('display',data);
 }
 
-
 app.post('/api/uploadfile', upload.single("uploadfile"), (req, res) =>{
+  //copy to diff directory once file come
+  var pathtowatch = __basedir + '/uploads/';
+  var oldpathwithfile = pathtowatch + req.file.filename;
+  var newpathwithfile = __basedir + '/python_file_writer/copy_' + req.file.filename;
+
+  watcher
+    .on('add', function(pathtowatch) {
+      console.log('File', pathtowatch, 'has been added');
+      fsex.copy(oldpathwithfile, newpathwithfile)
+    })
+    .on('change', function(pathtowatch) {
+      console.log('File', pathtowatch, 'has been changed');
+    })
+    .on('unlink', function(pathtowatch) {
+      console.log('File', pathtowatch, 'has been removed');
+    });
+
+  //Read using Node JS
   eventsEmitter.on('read',readFileContent);
   eventsEmitter.on('display',displayFileContent);
   eventsEmitter.on('finished',finished);
   eventsEmitter.emit('read',__basedir + '/uploads/' + req.file.filename);
-
 
     if (fs.existsSync(__basedir + '/uploads/' + req.file.filename)) {
         filePath = __basedir + '/uploads/' + req.file.filename;
@@ -92,4 +119,3 @@ app.post('/api/uploadfile', upload.single("uploadfile"), (req, res) =>{
 app.listen(8080, () => {
     console.log('PythonShell');
 })
-
